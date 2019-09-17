f601666c547f89dcd179f3cc55108b8640f61ff5
Hakon Hakonarson <hakonhakonarson@Hakons-Eplatolva.local>
2019-09-17 09:43:58 +0000

    Algorithm

--- tiles.py
+++ tiles.py
@@ -1,1 +1,15 @@
-#imamatest
+"""
+Gera fall fyrir hvert tile og skilgreina hvort hægt sé að fara north, south, west eða south. Nota IF commands sem segja hver maður getur farið
+Dæmi:
+
+def 1,1():
+    Print("you can travel: (N)orth.")
+    input = ("direction:")
+    if input == 'n' or input == 'N':
+        1,2()
+    else:
+        print("Not a valid direction")
+
+
+
+"""
