7c067d65dad5192de677a84c4f40346a1a04c035
Hakon Hakonarson <hakonhakonarson@Hakons-Eplatolva.local>
2019-09-17 10:15:57 +0000

    Good shit skeleton

--- tiles.py
+++ tiles.py
@@ -13,3 +13,43 @@ def 1,1():
 
 
 """
+def dog():
+    print("dog poop")
+def oneone():
+    print("You can travel: (N)orth.")
+    status = True
+    while status == True:
+        input_dir = input()
+        if input_dir == 'n' or input_dir == 'N':
+            status = False
+            onetwo()
+        else:
+            print("Not a valid direction!")
+
+def onetwo():
+    print("poop")
+
+def onethree():
+    print("poop")
+
+def twoone():
+    print("poop")
+
+def twotwo():
+    print("poop")
+
+def twotree():
+    print("poop")
+
+def threeone():
+    print("poop")
+
+def threetwo():
+    print("poop")
+
+def threethree():
+    print("poop")
+
+def inputdir():
+    direction = input('Direction: ')
+    return direction
